e3125b0c934aa1022480671cf01a190dcdcdd193
Olga_Ogarkova <hausfrau.sunlight@gmail.com>
2019-05-06 15:01:48 +0300

    init commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function playlist(state = [], action) {
+    if(action.type === 'ADD_TRACK') {
+        return [
+            ...state,
+            action.payload
+        ];
+    }
+    return state;
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(playlist);
+
+console.log(store.getState());
+
+store.subscribe(() => {
+    console.log('subscribe', store.getState());
+})
+
+store.dispatch({ type: 'ADD_TRACK', payload: 'Track-1' });
+store.dispatch({ type: 'ADD_TRACK', payload: 'Track-2' });
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+//
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
