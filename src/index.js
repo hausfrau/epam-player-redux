7397326f6c9c672d6d91dda4b32ec11c7ccb701f
Olga_Ogarkova <hausfrau.sunlight@gmail.com>
2019-05-07 14:54:14 +0300

    refactor: update tracks state.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function playlist(state = [], action) {
-    if(action.type === 'ADD_TRACK') {
-        return [
-            ...state,
-            action.payload
-        ];
+function playlist(state = {
+    tracks: [],
+    currentTrackId: 0
+}, action) {
+    if (action.type === 'ADD_TRACK') {
+        console.log(action);
+        return Object.assign({}, state,
+            {tracks: [...state.tracks, action.payload]}
+        );
     }
     return state;
 }
@@ -25,8 +28,8 @@ store.subscribe(() => {
     console.log('subscribe', store.getState());
 })
 
-store.dispatch({ type: 'ADD_TRACK', payload: 'Track-1' });
-store.dispatch({ type: 'ADD_TRACK', payload: 'Track-2' });
+store.dispatch({type: 'ADD_TRACK', payload: 'Track-1'});
+store.dispatch({type: 'ADD_TRACK', payload: 'Track-2'});
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 //
